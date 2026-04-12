71d3ae7f654df2e78b6aabf44fb028388eb5ef3c
echkiee3 <hardionkingparone2005@gmail.com>
2026-04-12 22:39:22 +0800

    Update addemployee.php

--- addemployee.php
+++ addemployee.php
@@ -1,1 +1,140 @@
-//jhfgjfdskjfjakds
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add Employee Info</title>
+    <style>
+form {
+  border-radius: 5px;
+  background-color: #f2f2f2;
+  padding: 20px;
+}
+
+label {display: block;}
+
+input[type=text], select {
+  width: 100%;
+  padding: 12px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
+
+input[type=submit] {
+  width: 100%;
+  background-color: #4CAF50;
+  color: white;
+  padding: 14px;
+  margin: 8px 0;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+input[type=submit]:hover {
+  background-color: #45a049;
+}
+</style>
+  </head>
+<?php
+include("webconnect.php");
+
+if(isset($_POST['Submit'])) 
+{
+
+$empnumber = $_POST['employeenumber'];
+$firstname = $_POST['firstname'];
+$mi= $_POST['mi'];
+$lname= $_POST['lastname'];
+
+//$lastname =  
+//$position =  
+
+
+$sql2 = " SELECT * from employees WHERE employeenumber = '$empnumber' AND lastname = '$lastname' AND firstname = '$firstname' AND mi = '$mi' ";
+$result = mysqli_query($conn, $sql2); 		
+//$row = mysqli_fetch_assoc($result);
+$count = mysqli_num_rows($result);
+
+if($count==0) {
+
+	$sql = "INSERT INTO employees (emp_num, fname, mname, lname) VALUES('$empnumber','$firstname','$mi','$lname'  )";
+	mysqli_query($conn, $sql);
+	?>
+	<SCRIPT Language=Javascript>
+	<!--
+	alert("Employee details has been successfully added!");
+	// End -->
+	</SCRIPT>
+
+	<SCRIPT Language=Javascript>
+	<!--
+      location.href="index.php";
+	// End -->
+	</SCRIPT>
+ <?php
+} else {
+?>
+
+<SCRIPT Language=Javascript>
+<!--
+	alert("Record Already exists. Please try again!");
+// End -->
+</SCRIPT>
+
+<SCRIPT Language=Javascript>
+<!--
+ location.href="addemployee.php";
+// End -->
+
+</SCRIPT>
+
+<?php
+  }
+ }
+
+
+?>
+<?php include("mainmenu.php") ; ?>
+  <body>
+    <div class="container">
+      <form name="myform" method="POST">
+      <label for="header"><h2>Add Employee Details</h2></label>
+        <label for="fname">First Name</label>
+        <input type="text" id="fname" name="firstname" placeholder="Employee Number">
+
+        <label for="fname">First Name</label>
+        <input type="text" id="firstname" name="firstname" placeholder="First Name">
+
+        <label for="middle_name">Middle Name</label>
+        <input type="text" id="mi" name="mi" placeholder="Middle Name">
+
+        <label for="lastname">Middle Name</label>
+        <input type="text" id="lastname" name="lastname" placeholder="Last Name">
+
+        <label for="address">Address</label>
+        <input type="text" id="address" name="address" placeholder="Address">
+        
+        <label for="gender">Gender</label>
+        <select id="gender" name="gender">
+          <option value="Male">Male</option>
+          <option value="Female">Female</option>
+        </select>
+
+        <label for="emp_status">Employment Status</label>
+        <input type="text" id="emp_status" name="emp_status" placeholder="Employment Status">
+        
+        <input type="submit" value="Submit">
+      </form>
+        <center>
+         <div class="signup-link"><a href="time_logs.php">Time Logs</a> | &nbsp;<a href="index.php">Home</a></div>
+     </div>
+ 
+  </body>
+
+</body>
+</html>
+
